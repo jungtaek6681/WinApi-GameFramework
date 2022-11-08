25347854293d7392de281661ed68f777080da0c0
jungtaek kim <kgajungtaek@gmail.com>
2022-11-09 01:39:41 +0900

    build : 초기 프로젝트 설정
    - 프로젝트 미리 컴파일된 해더 설정 변경
    - 프로젝트 포함, 라이브러리 경로 추가
    - 엔진 라이브러리 추가

--- WinApi/WinApi.cpp
+++ WinApi/WinApi.cpp
@@ -1,6 +1,7 @@
 ﻿// WinApi.cpp : 애플리케이션에 대한 진입점을 정의합니다.
 //
 
+#include "pch.h"
 #include "framework.h"
 #include "WinApi.h"
 
---
+++ WinApi/pch.h
@@ -0,0 +1,7 @@
+#pragma once
+
+// 게임 엔진
+#include "EnginePch.h"
+#include "Engine.h"
+
+#pragma comment(lib, "Engine.lib")
