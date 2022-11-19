4dfaae52025c4fc25c9cc94e7c2269a010cadc9f
jungtaek kim <kgajungtaek@gmail.com>
2022-11-19 13:42:44 +0900

    feat : 사운드 매니저 - 사운드 출력 구현
    사운드 매니저의 FMOD 사운드 제어 구현

--- Engine/CSoundManager.cpp
+++ Engine/CSoundManager.cpp
@@ -37,3 +37,88 @@ void CSoundManager::Release()
 		system->close();
 	}
 }
+
+void CSoundManager::PlayOnce(CSound* sound, float volume)
+{
+	FMOD_RESULT result;
+
+	result = sound->GetSound()->setMode(FMOD_LOOP_OFF);
+	assert(FMOD_OK == result && "Set Loop failed");
+
+	FMOD::Channel* channel;		// FMOD의 업데이트에서 모두 재생시 반환됨
+	result = system->playSound(sound->GetSound(), nullptr, false, &(channel));
+	assert(FMOD_OK == result && "Play sound failed");
+
+	result = channel->setVolume(volume);
+	assert(FMOD_OK == result && "Set volume failed");
+}
+
+void CSoundManager::PlayLoop(const wstring& soundName, CSound* sound, float volume)
+{
+	FMOD::Channel* channel = ChannelFind(soundName);
+	if (nullptr != channel)
+		Stop(soundName);
+
+	FMOD_RESULT result;
+
+	result = sound->GetSound()->setMode(FMOD_LOOP_NORMAL);
+	assert(FMOD_OK == result && "Set Loop failed");
+
+	result = system->playSound(sound->GetSound(), nullptr, false, &(channel));
+	assert(FMOD_OK == result && "Play sound failed");
+
+	result = channel->setVolume(volume);
+	assert(FMOD_OK == result && "Set volume failed");
+
+	loopChannels.insert(make_pair(soundName, channel));
+}
+
+void CSoundManager::Stop(const wstring& soundName)
+{
+	FMOD::Channel* channel = ChannelFind(soundName);
+	if (nullptr == channel)
+		return;
+
+	FMOD_RESULT result = channel->stop();
+	assert(FMOD_OK == result && "Stop sound failed");
+	loopChannels.erase(soundName);
+}
+
+void CSoundManager::StopAll()
+{
+	for (pair<wstring, FMOD::Channel*> channel : loopChannels)
+	{
+		FMOD_RESULT result = channel.second->stop();
+		assert(FMOD_OK == result && "Stop sound failed");
+	}
+	loopChannels.clear();
+}
+
+void CSoundManager::Pause(const wstring& soundName)
+{
+	FMOD::Channel* channel = ChannelFind(soundName);
+	if (nullptr == channel)
+		return;
+
+	FMOD_RESULT result = channel->setPaused(true);
+	assert(FMOD_OK == result && "SetPaused failed");
+}
+
+void CSoundManager::Resume(const wstring& soundName)
+{
+	FMOD::Channel* channel = ChannelFind(soundName);
+	if (nullptr == channel)
+		return;
+
+	FMOD_RESULT result = channel->setPaused(false);
+	assert(FMOD_OK == result && "SetResume failed");
+}
+
+FMOD::Channel* CSoundManager::ChannelFind(const wstring& soundName)
+{
+	auto iter = loopChannels.find(soundName);
+	if (iter == loopChannels.end())
+		return nullptr;
+	else
+		return iter->second;
+}
--- Engine/CSoundManager.h
+++ Engine/CSoundManager.h
@@ -9,14 +9,26 @@ private:
 public:
 	const static UINT MAXCHANNEL = 32;
 
-	void	Init();
-	void	Update();
-	void	Release();
+	void			Init();
+	void			Update();
+	void			Release();
 
-	System* GetSystem() { return system; }
+	void			PlayOnce(CSound* sound, float volume = 1.f);
+	void			PlayLoop(const wstring& soundName, CSound* sound, float volume = 1.f);
+
+	void			Stop(const wstring& soundName);
+	void			StopAll();
+
+	void			Pause(const wstring& soundName);
+	void			Resume(const wstring& soundName);
+
+	FMOD::Channel*	ChannelFind(const wstring& soundName);
+
+	System*			GetSystem() { return system; }
 
 private:
 	FMOD::System* system;
+	map<wstring, FMOD::Channel*> loopChannels;
 };
 
 #define SOUND	CSoundManager::GetInstance()
