b2be51ee72df63f2f2d66cd5b1d82da9190be7a8
jungtaek kim <kgajungtaek@gmail.com>
2022-11-20 20:52:04 +0900

    feat : 버튼 구현
    함수포인터와 람다식을 이용하여 버튼 클릭시 반응을 구현
    물론 상속하여 클릭시 반응을 재정의 할 수 있지만
    클릭시 반응만 다른 버튼들을 계속상속해서 만드는 경우는 비효율적
    반응만을 함수포인터와 람다식을 이용하여 객체 생성후 연결해 주는 방식으로 구현

--- WinApi/CSceneStage01.cpp
+++ WinApi/CSceneStage01.cpp
@@ -35,6 +35,31 @@ void CSceneStage01::Init()
 	panel->SetScale(Vec2(200, 200));
 	panel->SetPos(Vec2(100, 100));
 	AddUI(panel);
+
+	auto click1 = [](DWORD_PTR button1, DWORD_PTR param2) {
+		CButton* button = (CButton*)button1;
+		wstring text = button->GetName() + TEXT("이 클릭됨");
+		Logger::Debug(text);
+	};
+
+	CButton* button1 = new CButton();
+	button1->SetScale(Vec2(100, 50));
+	button1->SetPos(Vec2(400, 100));
+	button1->SetName(TEXT("버튼1"));
+	button1->SetClickCallback(click1, (DWORD_PTR)button1, 0);
+	AddUI(button1);
+
+	auto click2 = [](DWORD_PTR sceneType, DWORD_PTR param2) {
+		SceneType type = (SceneType)sceneType;
+		CAMERA->FadeOut(0.5f);
+		EVENT->ChangeScene(type, 0.5f);
+	};
+
+	CButton* button2 = new CButton();
+	button2->SetScale(Vec2(100, 50));
+	button2->SetPos(Vec2(500, 100));
+	button2->SetClickCallback(click2, (DWORD_PTR)SceneType::Title, 0);
+	AddUI(button2);
 }
 
 void CSceneStage01::Enter()
